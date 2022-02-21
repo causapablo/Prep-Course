5db0911d764054d534da24139145ac8f68365919
Pablo Causa <causapablo@gmail.com>
2022-02-21 17:44:56 -0300

    Homework

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -91,12 +91,26 @@ function asAmirror(str) {
   }).join(' ');
 } 
 
+String.prototype.reverse = function(){
+  var reversed = "";
+  for(var i=this.length-1;i>=0;i--){
+    reversed+=this[i];
+  }
+  return reversed;
+}
 
 function capicua(numero){
   //Escribe una función, la cual recibe un número y determina si es o no capicúa.
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var str = numero.toString();
+  if(str===str.reverse()){
+    return "Es capicua";
+  }else{
+    return "No es capicua";
+  }
+  
 }
 
 
